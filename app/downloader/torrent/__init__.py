56d67d0ebfb1ef88c8b0c75dbb0aca5beb1e6b51
Burhanverse <contact@burhanverse.eu.org>
2026-07-18 10:26:40 +0530

    refactor: update torrent module imports to reference core implementation
    Signed-off-by: Burhanverse <contact@burhanverse.eu.org>

--- app/downloader/torrent/__init__.py
+++ app/downloader/torrent/__init__.py
@@ -1,3 +1,3 @@
-from .torrent import download_torrent_async, start_aria2_daemon, stop_aria2_daemon
+from .core import download_torrent_async, start_aria2_daemon, stop_aria2_daemon
 
 __all__ = ["download_torrent_async", "start_aria2_daemon", "stop_aria2_daemon"]
